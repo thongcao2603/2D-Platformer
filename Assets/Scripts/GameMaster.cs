58bc2b1f1fc1a6002555b511a548e6233de23b61
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-10-16 17:36:26 +0700

    respawn player

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -1,9 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    public static GameMaster gm;
+
+    private void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    private int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer()
+    {
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
     public static void KillPlayer(Player player)
     {
         Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
     }
 }
